5af55e055d7eca0097773e445a397ad28d4c9ce9
Gia Hien Pham <hienhine0310@gmail.com>
2025-06-24 10:15:54 +0700

    install package

---
+++ lib/src/lets_draw_app.dart
@@ -0,0 +1,16 @@
+import 'package:drawing_board/src/presentation/pages/drawing_page.dart';
+import 'package:drawing_board/src/presentation/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+
+class LetsDrawApp extends StatelessWidget {
+  const LetsDrawApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Let/'s Draw",
+      theme: lightTheme,
+      home: const DrawingPage(),
+    );
+  }
+}
